6027d7defdf3017d52368cf3bc5eda807e4c729f
Yifan Lu <yifanlu@yifans-air.lan>
2024-12-28 10:20:18 -0800

    Updates

---
+++ precache-manifest.742564f0419363a5e9e5f6be29604048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e8aa12c3261bf11739b64511151c4b",
+    "url": "/YFL-dev/robofriends/index.html"
+  },
+  {
+    "revision": "e00fb64d6e842765674a",
+    "url": "/YFL-dev/robofriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "86f13550d97e8d4e819c",
+    "url": "/YFL-dev/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e00fb64d6e842765674a",
+    "url": "/YFL-dev/robofriends/static/js/2.3d5c1641.chunk.js"
+  },
+  {
+    "revision": "86f13550d97e8d4e819c",
+    "url": "/YFL-dev/robofriends/static/js/main.8ca8fbe0.chunk.js"
+  },
+  {
+    "revision": "b924315377ef884a862c",
+    "url": "/YFL-dev/robofriends/static/js/runtime~main.efcb021c.js"
+  }
+]);
